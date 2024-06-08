8ecacf8f8b7ddea5d9de1d4e5280d576773ee180
emonkhan500 <khdilo47@gmail.com>
2024-06-08 20:42:24 +0600

    token veryfy complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express= require('express')
 const app =express();
 const cors =require('cors')
-
+var jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port= process.env.PORT || 5000;
 
@@ -48,9 +48,29 @@ app.post('/jwt',async(req,res)=>{
 })
 
 
+
+// middleware
+const verifyToken= (req,res,next)=>{
+  // console.log( 'inside verify',req.headers.authorization)
+
+  if(!req.headers.authorization){
+return res.status(401).send({message: 'unauthorized access'})
+  }
+  const token= req.headers.authorization.split(' ')[1]
+ jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+  if(err){
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+  req.decoded= decoded
+  next()
+ }) 
+}
+
+
+
 // user related apis
 
-app.get('/users', async (req,res)=>{
+app.get('/users',verifyToken, async (req,res)=>{
   const result= await userCollection.find().toArray();
   
   res.send(result)
