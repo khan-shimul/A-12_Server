9f6714626ca1449c70792fd538250aa9dc85a0e2
emonkhan500 <khdilo47@gmail.com>
2024-06-08 15:27:17 +0600

    user get & delete api created

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hzfjxhp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,6 +32,51 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    const userCollection =client.db('12RealEstate').collection('user')
+
+
+
+
+
+
+
+// Admin apies
+
+
+
+
+
+// user related apis
+
+app.get('/users', async (req,res)=>{
+  const result= await userCollection.find().toArray();
+  
+  res.send(result)
+})
+
+    app.post('/users',async(req,res)=>{
+      const user = req.body
+      const query= {email:user.email}
+      const existingUser= await userCollection.findOne(query)
+      if(existingUser){
+        return res.send({message:'user already exist'})
+      }
+      const result = await userCollection.insertOne(user)
+      res.send(result)
+    
+    })
+
+
+    app.delete('/users/:id',async(req,res)=>{
+      const id = req.params.id
+      const query= {_id: new ObjectId(id)}
+      const result= await userCollection.deleteOne(query)
+      res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
