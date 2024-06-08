d4c479184c3423fc2d8df6f9faa306af8f34cee3
emonkhan500 <khdilo47@gmail.com>
2024-06-08 22:57:48 +0600

    agent verify created

--- index.js
+++ index.js
@@ -66,13 +66,25 @@ return res.status(401).send({message: 'unauthorized access'})
  }) 
 }
 
-
+// admin verify
 const verifyAdmin= async(req,res,next)=>{
   const email= req.decoded.email
   const query ={email: email}
   const user= await userCollection.findOne(query)
   const isAdmin= user?.role === 'admin'
   if(!isAdmin){
+    return res.status(403).send({message: 'forbidden access'})
+  }
+  next()
+}
+
+// agent verify
+const verifyAgent= async(req,res,next)=>{
+  const email= req.decoded.email
+  const query ={email: email}
+  const user= await userCollection.findOne(query)
+  const isAgent= user?.role === 'agent'
+  if(!isAgent){
     return res.status(403).send({message: 'forbidden access'})
   }
   next()
